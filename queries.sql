9b19bf129d3587d3c517d56510b40e96a0983180
alessandroschiano98 <schianoski98@gmail.com>
2025-03-30 16:05:51 +0200

    vari fix diagramma

---
+++ queries.sql
@@ -0,0 +1,16 @@
+1. Seleziona gli utenti che hanno postato almeno un video
+
+
+2. Seleziona tutti i post senza Like (13)
+
+
+3. Conta il numero di like per ogni post (165) nel caso di query con SELECT + JOIN
+
+
+oppure 175 nel caso di query con sola SELECT
+
+
+4. Ordina gli utenti per il numero di media caricati (25)
+
+
+5. Ordina gli utenti per totale di likes ricevuti nei loro posts (25)
