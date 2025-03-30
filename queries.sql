b697a8d13f8263fc515c1c012b2eae83b44f2e22
alessandroschiano98 <schianoski98@gmail.com>
2025-03-30 16:38:54 +0200

    FIX tabella ponte diagramma

--- queries.sql
+++ queries.sql
@@ -1,13 +1,17 @@
 1. Seleziona gli utenti che hanno postato almeno un video
+FROM `users`
+INNER JOIN `medias`
+ON `users`.`id` = `medias`.`user_id`
+WHERE `medias`.`type`>= 1
+AND `medias`.`type` = "video";
+
+
 
 
 2. Seleziona tutti i post senza Like (13)
 
 
-3. Conta il numero di like per ogni post (165) nel caso di query con SELECT + JOIN
-
-
-oppure 175 nel caso di query con sola SELECT
+3. Conta il numero di like per ogni post (165) nel caso di query con SELECT + JOIN oppure 175 nel caso di query con sola SELECT
 
 
 4. Ordina gli utenti per il numero di media caricati (25)
